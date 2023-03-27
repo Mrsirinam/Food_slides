87101e2f35eef05e9662091bd578b9b08f4d9b4a
Mrsirinam <myworkmail2016@mail.ru>
2023-03-27 22:09:47 +0300

    change active slide

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-const slides = document.querySelectorAll('.slide')
 
-for (const slide of slides) {
-	slide.addEventListener('click', () => {
-		clearActiveClasses()
-		
-		slide.classList.add('active')
-	})
+function slidesPlugin(activeSlide = 2) {
+	const slides = document.querySelectorAll('.slide')
+
+	slides[activeSlide].classList.add('active')
+	for (const slide of slides) {
+		slide.addEventListener('click', () => {
+			clearActiveClasses()
+			
+			slide.classList.add('active')
+		})
+	}
+	
+	function clearActiveClasses() {
+		slides.forEach((slide) => {
+			slide.classList.remove('active')
+		})
+	}
 }
 
-function clearActiveClasses() {
-	slides.forEach((slide) => {
-		slide.classList.remove('active')
-	})
-}
+slidesPlugin(2)
